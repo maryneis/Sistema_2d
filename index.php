316ffdeece9d3dc7e0697afa5603d8a029436328
maduvy <129520532+maryneis@users.noreply.github.com>
2023-09-25 11:10:20 -0300

    t2

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link rel="stylesheet" href="estilos/estilo.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div id="corpo">
+    <h1>escolha um jogo</h1>
+    <table class="listagem">
+        <tr><td>Nome <td>Foto <td> Adm
+        <tr><td>Nome <td>Foto <td> Adm 
+        <tr><td>Nome <td>Foto <td> Adm
+        <tr><td>Nome <td>Foto <td> Adm
+</table>
+</div>
+
+    
+</body>
+</html>
